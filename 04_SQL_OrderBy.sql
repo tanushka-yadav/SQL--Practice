46ea8ef3844f1c42236f032e3d86b53c6f261726
Tanushka Yadav <tanushkayadav766@gmail.com>
2026-07-24 08:48:14 -0700

    Complete SQL Lesson 4 - ORDER BY

--- 04_SQL_OrderBy.sql
+++ 04_SQL_OrderBy.sql
@@ -33,3 +33,43 @@ select * from customer order by contactName DESC;
 
 -- Challenge 3
 select * from customer order by city, customerName;
+
+-- Professional ORDER BY Queries
+-- Task 1- Sort Customers Alphabetically
+select * from customer order by customerName;
+
+-- Task 2- Reverse Alphabetical Order
+select * from customer order by customerName desc;
+
+-- Task 3 - Sort by City
+select * from customer order by city;
+
+-- Task 4 - Sort by Postal Code
+select * from customer order by postalCode;
+
+-- Task 5- Highest Postal Code first
+select * from customer order by postalCode desc;
+
+-- Task 6- Multiple Column Sorting
+select * from customer order by country, city, customerName;
+
+-- Task 7- Mixed Sorting
+select * from customer order by city asc, customerName desc;
+
+-- Task 8 - Display Selected Columns
+select customerName, city, country from customer order by city, customerName;
+
+-- Challenge 1
+select customerName, city from customer order by customerName;
+
+-- Challenge 2
+select distinct city from customer order by city desc;
+
+-- Challenge 3
+select customerName, country from customer order by country, customerName;
+
+-- Challenge 4
+select customerName, address, city from customer order by city desc;
+
+-- Challenge 5
+select customerName, contactName, city, country from customer order by country, city, customerName;
